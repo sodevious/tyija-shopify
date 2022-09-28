0763e463f1fdcfd4241370281bbac812a4a12558
Nicole Dominguez <sodevious.net@gmail.com>
2022-09-28 15:23:27 -0400

    fix wishlist from breaking

--- src/vue/components/WishlistButton.vue
+++ src/vue/components/WishlistButton.vue
@@ -1,13 +1,13 @@
 <template>
   <button
     class="hover:text-red"
-    @click="toggleInWishList"
+    @click="getProductData"
   >
     <svg
       enable-background="new 0 0 28.2 25.6"
       viewBox="0 0 28.2 25.6"
       xmlns="http://www.w3.org/2000/svg"
-      class="w-8 h-8"
+      class="w-7 h-7"
     >
       <path
         d="m14.5 23.6-1.6-1.4c-5.7-5.1-9.4-8.4-9.4-12.6 0-3.4 2.7-6 6.1-6 1.9 0 3.8.9 4.9 2.3 1.2-1.4 3-2.3 5-2.3 3.4 0 6 2.6 6 6 0 4.1-3.7 7.5-9.4 12.6z"
@@ -22,43 +22,15 @@
 export default {
   name: 'WishlistButton',
   props: {
-    rawProductData: Object,
+    productHandle: String,
+    productId: String,
     productLink: String,
     swymProductData: Object
   },
   data() {
     return {
-      isProductAdded: false
-    }
-  },
-  computed: {
-    getProductData() {
-
-      if (this.swymProductData) {
-        return {
-          'epi': this.swymProductData.epi,
-          'empi': this.swymProductData.empi,
-          'du': this.swymProductData.du,
-          'iu': this.swymProductData.iu,
-          'pr': this.swymProductData.pr
-        }
-      } else {
-
-        let data = JSON.parse(this.rawProductData)
-
-        return {
-          'epi': data.variants[0].id,
-          'empi': data.id,
-          'du': this.productLink,
-          'iu': data.featured_image,
-          'pr': data.price / 100,
-          'cprops': {
-            'productPath': this.productLink,
-            'productTitle': data.title,
-            'productVendor': data.vendor
-          }
-        }
-      }
+      isProductAdded: false,
+      rawData: Object
     }
   },
   mounted() {
@@ -69,9 +41,29 @@ export default {
     this.checkProductInTheList()
   },
   methods: {
-    toggleInWishList() {
+    getProductData() {
+      fetch(`/products/${this.productHandle}/${this.productId}.json`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.toggleInWishList(data)
+        })
+    },
+    toggleInWishList(data) {
+      let productData = {
+        'epi': data.product.variants[0].id,
+        'empi': data.product.id,
+        'du': this.productLink,
+        'iu': this.productImage,
+        'pr': data.product.variants[0].price,
+        'cprops': {
+          'productPath': this.productLink,
+          'productTitle': data.product.title,
+          'productVendor': data.product.vendor
+        }
+      }
+
       const addToWishList = (swat) => {
-        swat.addToWishList(this.getProductData, (r) => {
+        swat.addToWishList(productData, (r) => {
           this.isProductAdded = true
         })
       }
@@ -79,8 +71,8 @@ export default {
       const removeFromWishList = (swat) => {
         const productDataWithoutCprops = {}
         // make a copy without cprops
-        Object.keys(this.getProductData).filter(key => key !== 'cprops').forEach((key) => {
-          productDataWithoutCprops[key] = this.getProductData[key]
+        Object.keys(productData).filter(key => key !== 'cprops').forEach((key) => {
+          productDataWithoutCprops[key] = productData[key]
         })
 
         swat.removeFromWishList(productDataWithoutCprops, (r) => {
@@ -93,10 +85,11 @@ export default {
         : window.SwymCallbacks.push(addToWishList)
     },
     checkProductInTheList() {
+      console.log('check')
       window.SwymCallbacks.push((swat) => {
         swat.fetch((wishlist) => {
           wishlist.forEach((item) => {
-            if (item.epi == this.getProductData.epi) {
+            if (item.epi == this.productId) {
               this.isProductAdded = true
             }
           })
